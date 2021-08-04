7b18aebe38710d03802e98adb616999609604b74
Nicholas Radtke <Raddnc@gmail.com>
2021-08-04 17:04:08 -0500

    finishing header, working on making portfolio easy to update by creating 1 file to pull display text from

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,62 @@
-import React from "react";
+import React, { useContext } from "react";
+import "./Header.css";
 import Headroom from "react-headroom";
+import {
+  greeting,
+  workExperiences,
+  skillsSection,
+  openSource,
+  myProjects,
+} from "../../portfolio";
 
 function Header() {
+  const viewExperience = workExperiences.display;
+  const viewOpenSource = openSource.display;
+  const viewSkills = skillsSection.display;
+  const viewMyProjects = myProjects.display;
   return (
     <Headroom>
-      <h1>LETS GO</h1>
+      <header className="header">
+        <a href="/" className="logo">
+          <span className="logo_name">{greeting.username}</span>
+        </a>
+        <input className="menu_btn" type="checkbox" id="menu_btn" />
+        <label
+          className="menu_icon"
+          htmlFor="menu_btn"
+          style={{ color: "white" }}
+        >
+          <span className="navicon"></span>
+        </label>
+        <ul className="menu">
+          {viewSkills && (
+            <li>
+              <a href="#skills">Skills</a>
+            </li>
+          )}
+          {viewExperience && (
+            <li>
+              <a href="#experience">Work Experiences</a>
+            </li>
+          )}
+          {viewOpenSource && (
+            <li>
+              <a href="#opensource">Open Source</a>
+            </li>
+          )}
+          {viewMyProjects && (
+            <li>
+              <a href="#myprojects">My Projects</a>
+            </li>
+          )}
+          <li>
+            <a href="#contact">Contact Me</a>
+          </li>
+          <li>
+            <a href="#">{/* <ToggleSwitch /> */}</a>
+          </li>
+        </ul>
+      </header>
     </Headroom>
   );
 }
