7edff0932cba0a1ade5a4e2bb519b7dc3837ded5
Nicholas Radtke <Raddnc@gmail.com>
2021-08-04 15:30:49 -0500

    fixed yarn issue

---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Headroom from "react-headroom";
+
+function Header() {
+  return (
+    <Headroom>
+      <h1>LETS GO</h1>
+    </Headroom>
+  );
+}
+
+export default Header;
---
+++ src/container/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/header/Header";
+//import Footer from "../components/footer/Footer";
+function Main() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default Main;
