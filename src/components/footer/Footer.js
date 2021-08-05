6e285dee5dce0ff65fa76cbb898d5944322d6f45
Nicholas Radtke <Raddnc@gmail.com>
2021-08-04 19:21:54 -0500

    added fading effect

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,14 +1,18 @@
 import React, { useContext } from "react";
 import "./Footer.css";
 import StyleContext from "../../contexts/StyleContext";
+import { Fade } from "react-reveal";
+
 function Footer() {
   const { isDark } = useContext(StyleContext);
   return (
-    <div className="footer">
-      <p className={isDark ? "dark_mode footer_text" : "footer_text"}>
-        <a href="https://github.com/NCRadtke00">Made by Nick Radtke</a>
-      </p>
-    </div>
+    <Fade bottom duration={1000} distance="5px">
+      <div className="footer">
+        <p className={isDark ? "dark_mode footer_text" : "footer_text"}>
+          <a href="https://github.com/NCRadtke00">Made by Nick Radtke</a>
+        </p>
+      </div>
+    </Fade>
   );
 }
 
